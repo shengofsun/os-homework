19e6ec183c923526ef9b5192d5e879cbd0dd7ded
lucky-sun <luckyweijie@gmail.com>
2011-09-14 13:04:07 +0800

    continue

--- fs/src/fs.c
+++ fs/src/fs.c
@@ -5,7 +5,7 @@
 
 #define MAX_FD 256
 
-static const char magic[] = "\0221\0124f";
+static const char magic[] = "\0221\012";
 #define blocksz 4096
 #define inodect (blocksz/64)
 #define FREE_BLOCK_NUM 500
@@ -59,9 +59,14 @@ typedef struct dentry {
     int inode;
 } dentry;
 
+static int openi(fs*, const char*, int);
+
 static int writeblk(fs * f, int bid) {
-    fseek(f->fp, f->sb.block_offset + bid * blocksz, SEEK_SET);
-    return fwrite(f->buf[bid].d, blocksz, 1, f->fp);
+    int ret;
+    fseek(f->fp, f->sb.block_offset + f->buf[bid].bid * blocksz, SEEK_SET);
+    ret = fwrite(f->buf[bid].d, blocksz, 1, f->fp);
+//    printf("%d %d\n", bid, ret);
+    return ret;
 }
 
 static buffer* openblk(fs * f, int bid) {
@@ -82,6 +87,9 @@ static buffer* openblk(fs * f, int bid) {
         }
         f->buf[k].dirty = 0;
     }
+    f->buf[k].bid = bid;
+    fseek(f->fp, f->sb.block_offset + bid * blocksz, SEEK_SET);
+    fread(f->buf[k].d, blocksz , 1, f->fp);
     return &f->buf[k];
 }
 
@@ -162,8 +170,8 @@ static fs * new_fs() {
         f->buf[i].bid = -1;
     }
     f->dno = 1;
-    f->cdir[1] = '/';
-    f->cdir[2] = '\0';
+    f->cdir[0] = '/';
+    f->cdir[1] = '\0';
     return f;
 }
 
@@ -171,7 +179,7 @@ static int bmap(fs *f, int ip, int bn){
     const int ic = blocksz/sizeof(int);
     if (bn < 0 || bn >= ic*8) return -1;
     inode* inode = &f->inodes[ip];
-    if (!(inode->mode&2) && bn > 8) {
+    if (!(inode->mode&2) && bn >= 8) {
         buffer *bp = openblk(f, alloc_blk(f));
         memset(bp->d, 0, sizeof(bp->d));
         bp->dirty = 1;
@@ -185,7 +193,7 @@ static int bmap(fs *f, int ip, int bn){
                               (inode->block_id[bn]=alloc_blk(f));
     
     buffer* bp = openblk(f, bn/ic);
-    int *ptr = bp->d;
+    int *ptr = (int*)bp->d;
     if (!ptr[bn%ic]){
         bp->dirty = 1;
         ptr[bn%ic] = alloc_blk(f);
@@ -224,8 +232,10 @@ static int writei(fs *f, int ip, int off, const void* ptr, int size){
         buffer* bp = openblk(f, bmap(f, ip, off/blocksz));
         bp->dirty = 1;
         memcpy(bp->d, src, size);
+        off += size;
     }
 
+    if (f->inodes[ip].size < off) f->inodes[ip].size = off;
     return ret;
 }
 
@@ -272,97 +282,6 @@ static void add_entry(fs * f, int to, const char* str, int id) {
     ++f->inodes[to].dcnt;
 }
 
-static int split_dir(char* path, const char* p[])
-{
-    int off = 0;
-
-    if ( *path==0 ) return off;
-    
-    p[off++]=path;
-    while (*path) {
-        if (*path=='/') {
-            p[off++]=path+1;
-            *path=0;
-        }
-        path++;
-    }
-    return off;
-}
-
-static int openi(fs* f, const char* path, int create_flag)
-{
-    fs_dir* dir;
-    dentry entry;
-    char full_path[500];
-    char* p[50];
-
-    int count = split_dir(full_path+1, p);
-    int this_inode = 0, offset, i=0, bytes_get, father_inode;
-
-    sprintf(full_path, "%s/%s", f->cdir, path);
-    format(full_path);
-
-    if ( count<=0 ) return -1;
-    
-    while ( i<count )
-    {
-        offset = 0;
-        while ( (bytes_get = readi(f, this_inode, offset, &entry, sizeof(entry))) )
-            if ( strcmp(p[i], entry.fname)==0 ) break;
-        if (bytes_get>0){
-            father_inode = this_inode;
-            this_inode = entry.inode;
-            i++;
-        }
-        else break;
-    }
-    if ( i>=count ) {
-        return (create_flag==2)?father_inode:this_inode;
-    }
-    if ( i+1>=count && create_flag ){
-        i = f->sb.free_inode; 
-        if ( i==0 ) return -1;
-        else {
-            f->sb.free_inode = f->inodes[i].next_id;
-            add_entry(f, father_inode, p[count-1], i);
-            return i;
-        }
-    }
-    return -1;
-}
-
-static int init_super_block(fs * f, int nblk, int ninode) {
-    superblock * sb;
-    int i;
-    int ret;
-
-    sb = &f->sb;
-    
-    // format super block:
-    // 1. set_magic_number:
-    memcpy(sb->magic_number, magic, sizeof(sb->magic_number));
-
-    // 2. init inode
-    sb->inode_cnt = ninode;
-    memset(f->inodes, 0, sizeof(inode) * ninode);
-    sb->free_inode = 1;
-    for (i = 2; i < ninode; ++i)
-        f->inodes[i-1].next_id = i;
-    // init root dir:
-    f->inodes[0].ref_count = 255;
-    add_entry(f, 0, ".", 0);
-    add_entry(f, 0, "..", 0);
-        
-    // 3. init blk
-    ret = 1;
-    sb->total_free_block_num = 0;
-    sb->block_cnt = 0;
-    sb->block_offset = blocksz + (ninode / inodect);
-    for (i = 0; ret == 1 && i < nblk; ++i)
-        ret = free_blk(f, i);
-    return ret;
-}
-
 static int backwards(const char* buf, int last, int cnt)
 {
     if ( last<=0 ) return 0;
@@ -405,40 +324,134 @@ static void format_path(char *buf)
                     state = 0;
                     buf[last++]=buf[i++];
                 }
-        }
+    }
+
+    if ( state==2 )
+        last = backwards(buf, last-1, backs);
+    if (last==0) last++;
     buf[last]=0;
 }
 
-static fs_dir* opendiri(fs *f,  int inode) {
-    if (!(f->inodes[inode].mode&1)) return 0;
-    fs_dir* ret = malloc(sizeof(fs_dir));
-    ret->cur_off = 0;
-    ret->inode = inode;
-    return ret;
+static int split_dir(char* path, char* p[])
+{
+    int off = 0;
+
+    if ( *path==0 ) return off;
+    
+    p[off++]=path;
+    while (*path) {
+        if (*path=='/') {
+            p[off++]=path+1;
+            *path=0;
+        }
+        path++;
+    }
+    return off;
 }
 
-static int nextent(fs *f, fs_dir* dir, dentry *ent){
-    if (f->inodes[dir->inode].dcnt >= dir->cur_off) return -1;
-    readi(f, dir->inode, sizeof(dentry)*dir->cur_off, dir, sizeof(dentry));
-    return 0;
-}
+/*
+ *open inode by the path. relative path is supported.
+ *If the create_flag is 0, then -1 will be returned if the path not exist.
+ *if the create_flag is 1, a new file will be created if the last level of the path not exist.
+ *If the create_flag is 2, father's inode will be returned.
+ * */
+static int openi(fs* f, const char* path, int create_flag)
+{
+    dentry entry;
+    char full_path[500];
+    char* p[50];
 
-static int findindir(fs *f, int inode, const char * entname) {
-    fs_dir * dir = opendiri(f, inode);
-    dentry ent;
-    while (!nextent(f, dir, &ent)) {
-        if (strcmp(ent.fname, entname) == 0) {
-            return ent.inode;
+    int count, this_inode = 0, offset, i=0, bytes_get, father_inode = 0;
+
+    if (path[0] == '/')
+        strcpy(full_path, path);
+    else
+        sprintf(full_path, "%s/%s", f->cdir, path);
+    format_path(full_path);
+
+    count = split_dir(full_path+1, p);
+    if ( count<=0 ) return 0;
+    
+    while ( i<count )
+    {
+        offset = 0;
+        while ( (bytes_get = readi(f, this_inode, offset, &entry, sizeof(entry))) ) {
+            if ( strcmp(p[i], entry.fname)==0 ) break;
+            offset += sizeof(entry);
+        }
+        if (bytes_get>0){
+            father_inode = this_inode;
+            this_inode = entry.inode;
+            i++;
+        }
+        else break;
+    }
+    if ( i>=count ) {
+        return (create_flag==2)?father_inode:this_inode;
+    }
+    if ( i+1>=count && create_flag ){
+        if (create_flag == 2) return this_inode;
+        i = f->sb.free_inode; 
+        if ( i==0 ) return -1;
+        else {
+            f->sb.free_inode = f->inodes[i].next_id;
+            add_entry(f, this_inode, p[count-1], i);
+            return i;
         }
     }
     return -1;
+}
+
+static int init_super_block(fs * f, int nblk, int ninode) {
+    superblock * sb;
+    int i;
+    int ret;
+
+    sb = &f->sb;
+    
+    // format super block:
+    // 1. set_magic_number:
+    memcpy(sb->magic_number, magic, sizeof(sb->magic_number));
+
+    // 2. init blk
+    ret = 1;
+    sb->total_free_block_num = 0;
+    sb->block_cnt = 0;
+    sb->block_offset = blocksz * (1 + ninode / inodect) ;
+    for (i = 0; ret == 1 && i < nblk; ++i)
+        ret = free_blk(f, i);
+
+    // 3. init inode
+    sb->inode_cnt = ninode;
+    memset(f->inodes, 0, sizeof(inode) * ninode);
+    sb->free_inode = 1;
+    for (i = 2; i < ninode; ++i)
+        f->inodes[i-1].next_id = i;
+    
+    // init root dir:
+    f->inodes[0].mode |= 1;
+    f->inodes[0].ref_count = 255;
+    add_entry(f, 0, ".", 0);
+    add_entry(f, 0, "..", 0);
+        
+    return ret;
+}
+
+static fs_dir* opendiri(fs *f,  int inode) {
+    if (inode == -1) return 0;
+    if (!(f->inodes[inode].mode&1)) return 0;
+    fs_dir* ret = malloc(sizeof(fs_dir));
+    ret->f = f;
+    ret->cur_off = 0;
+    ret->inode = inode;
+    return ret;
 }
 
 static void remove_entry(fs* f, int father_inode, int son_inode)
 {
     dentry entry;
     int offset = 0, bytes_get;
-    while ( bytes_get = readi(f, father_inode, offset, &entry, sizeof(entry)) ){
+    while ( (bytes_get = readi(f, father_inode, offset, &entry, sizeof(entry)))>0 ){
         if ( entry.inode == son_inode )
             break;
         offset += sizeof(entry);
@@ -457,6 +470,7 @@ static void remove_entry(fs* f, int father_inode, int son_inode)
 fs * fs_creatfs(const char* fname, int block_num, int inode_num) {
     fs * f;
     int i;
+
     if (block_num <= 10) return NULL;
     if (fname == NULL) return NULL;
     if (inode_num == -1) inode_num = block_num / 10;
@@ -481,6 +495,15 @@ fs * fs_creatfs(const char* fname, int block_num, int inode_num) {
         free(f->inodes);
         free(f);
         return NULL;
+    }
+
+    fseek(f->fp, 0, SEEK_SET);
+    for (i = 0; i < f->sb.block_offset + block_num * blocksz; i+=blocksz) {
+        if (!fwrite(f->buf[0].d, blocksz, 1, f->fp)) {
+            free(f->inodes);
+            free(f);
+            return NULL;
+        }
     }
     
     return f;
@@ -517,6 +540,9 @@ void fs_closefs(fs *f) {
     for (i = 0; i < 16; ++i)
         if (f->buf[i].dirty)
             writeblk(f, i);
+    fseek(f->fp, 0, SEEK_SET);
+    fwrite(&f->sb, blocksz, 1, f->fp);
+    fwrite(f->inodes, sizeof(inode)*f->sb.inode_cnt, 1, f->fp);
     fclose(f->fp);
     free(f->inodes);
     free(f);
@@ -530,6 +556,7 @@ void fs_pwd(fs* f, char* buf, size_t buf_len) {
     int len = strlen(f->cdir);
     if (buf_len < len) len = buf_len;
     memcpy(buf, f->cdir, len);
+    buf[len]=0;
 }
 
 int fs_chdir(fs* f, const char* dir) {
@@ -537,12 +564,13 @@ int fs_chdir(fs* f, const char* dir) {
     if (dir[0] == '/')
         strcpy(buf, dir);
     else
-        sprintf("%s/%s", f->cdir, dir);
+        sprintf(buf, "%s/%s", f->cdir, dir);
     format_path(buf);
-    int dn = openi(f, buf);
+    int dn = openi(f, buf, 0);
     if (dn == -1 || ((f->inodes[dn].mode & 1) == 0)) return -1;
     f->dno = dn;
-    strcpy(f->cdir, buf);    
+    strcpy(f->cdir, buf);
+    return 1;
 }
 
 int fs_open(fs* f, const char* fname, int mode) {
@@ -563,7 +591,7 @@ int fs_open(fs* f, const char* fname, int mode) {
         else {
             if ((mode & FS_WRITE) && (mode & FS_APPEND) == 0) { // remove current file
                 f->fds[k].inodeid = -1;
-                fs_remove(fs, fname);
+                fs_remove(f, fname);
             }
         }
 
@@ -637,53 +665,84 @@ int fs_remove(fs* f, const char* path) {
     return 0;
 }
 
+/* static char* find_parent_and_son(char* path) */
+/* { */
+/*     int len = strlen(path); */
+/*     if ( len < 0 ) return NULL; */
+/*     if ( path[len -1 ]=='/' ) path[--len]=0; */
+/*     while ( path[len]!='/' && len>=0 ) len--; */
+/*     if ( len<0 ) return NULL; */
+
+/*     path[len]=0; */
+/*     return path+len+1; */
+/* } */
+
+int fs_mkdir(fs* f, const char* path)
+{
+    char full_path[500];
+    
+    int ino_parent, new_inode;
+    
+    sprintf(full_path, "%s/%s", f->cdir, path);
+    format_path(full_path);
+
+    if (openi(f, full_path, 0) != -1) return -1;
+    if ((ino_parent = openi(f, full_path, 2)) == -1) return -1;
+    if ((new_inode = openi(f, full_path, 1)) == -1) return -1;
+
+    f->inodes[new_inode].mode |= 1;
+    
+    add_entry(f, new_inode, ".", new_inode);
+    add_entry(f, new_inode, "..", ino_parent);
+
+    return 1;
+}
+
 int fs_removedir(fs* f, const char* dir) {
     int ino;
     int fno;
-    ino = openi(f, path, 0);
+    ino = openi(f, dir, 0);
     if (ino == -1 || 
         (f->inodes[ino].mode & 1) == 0 ||
         f->inodes[ino].dcnt > 0) {
         return -1;
     }
-    fno = openi(f, path, 2);
+    fno = openi(f, dir, 2);
     if (fno == -1) return -1;
     release_inode_blk(f, ino);
     remove_entry(f, fno, ino);
     return 0;
 }
 
-fs_dir * fs_opendir(fs* f, const char* dir) {
-    fs_dir * dir = malloc(sizeof(*dir));
-    dir->inode = openi(f, dir, 0);
-    if (dir->inode == -1) {
-        free(dir);
-        return NULL;
-    }
-    dir->f = f;
-    dir->cur_off;
-    return dir;
+fs_dir * fs_opendir(fs* f, const char* path) {
+    /* fs_dir * dir = malloc(sizeof(fs_dir)); */
+    /* dir->inode = openi(f, path, 0); */
+    /* if (dir->inode == -1) { */
+    /*     free(dir); */
+    /*     return NULL; */
+    /* } */
+    /* dir->f = f; */
+    /* dir->cur_off=0; */
+    /* return dir; */
+    return opendiri(f, openi(f, path, 0));
 }
 
 int fs_nextent(fs_dir* dir, char* buf, size_t buf_len) {
     fs * f = dir->f;
     dentry ent;
     int len;
-    if (dir->cur_off >= f->inodes[dir->inode].dentry * sizeof(dentry)) { 
+    if (dir->cur_off >= f->inodes[dir->inode].dcnt * sizeof(dentry)) { 
         return 0;
     }
-    readi(*f, dir->inode, dir->cur_off, &ent, sizeof(ent));
+    readi(f, dir->inode, dir->cur_off, &ent, sizeof(ent));
+    dir->cur_off += sizeof(ent);
     len = strlen(ent.fname);
-    if (buf_len < len) len = buf_len;
-    memcpy(buf, ent.fname, len - 1);
-    buf[len-1] = '\0';
+    if (buf_len - 1 < len) len = buf_len - 1;
+    memcpy(buf, ent.fname, len);
+    buf[len] = '\0';
     return 1;
 }
 
 void fs_closedir(fs_dir* dir) {
     free(dir);
-}
-
-int main(){
-    return 0;
 }
--- shell/main.c
+++ shell/main.c
@@ -1,48 +1,68 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "../fs/include/fs.h"
 
 fs* filesys;
 static char workdir[1000];
 
-static void ls_this_dir(const char* dir_name, int hidden)
+static void help(const char* entry)
+{
+    if ( strcmp(entry, "main")==0 )
+    {
+        printf("Simple fs. By our yying and hard working.\n");
+        printf("Usage:\n");
+        printf("1. fs [file name] \n");
+        printf("This command open a file as an file system\n");
+        printf("2. fs [options]\n");
+        printf("This command create a new file system.\n");
+        printf("options:\n");
+        printf("-f|--format filename --size|-s count1 --inodes|-i count2");
+        printf("the new file system's file name is filename.\nAnd the size is the cout1. inode size is count2\n");
+    }
+}
+
+static void ls_this_dir(const char* dir_name, int hide)
 {
     static char entry[1000];
     fs_dir* fd = fs_opendir(filesys, dir_name);
+
+    if ( fd==NULL )
+    {
+        printf("Directory %s not exist\n", dir_name);
+        return;
+    }
     
-    printf("%s\n", dir_name);
+    printf("Directory: %s\n", dir_name);
     
     while ( fs_nextent(fd, entry, 999) )
     {
-        if ( hidden==0 || entry[0]!='.' )
+        if ( hide==0 || entry[0]!='.' )
             printf("%s\n", entry);
     }
-    
-    printf("\n");
 
     fs_closedir(fd);
 }
 
 void ls(char* params[], int p_cnt)
 {
-    int hidden = 0, i;
-    char* dir==NULL;
+    int hide = 1, i;
     int dir_cnt = 0;
     
     for (i=0; i<p_cnt; i++)
         if ( strcmp(params[i], "-a")==0 )
-            hidden = 1;
+            hide = 0;
 
     for (i=0; i<p_cnt; i++)
     {
         if ( params[i][0]!='-' ){
-            ls_this_dir(params[i], hidden);
+            ls_this_dir(params[i], hide);
             dir_cnt ++;
         }
     }
 
     if ( dir_cnt==0 )
-        ls_this_dir(workdir);
+        ls_this_dir(workdir, hide);
 }
 
 void cd(char* params[], int len)
@@ -83,36 +103,39 @@ static void remove_entry(const char* path)
         return;
     }
     
-    if ( fstat(filesys, fd, ibuffer) == -1 ){
+    if ( fs_fstat(filesys, fd, &ibuffer) == -1 ){
         printf("error occured.\n");
         fs_close(filesys, fd);
         return;
     }
 
-    if ( ibuffer.mode&1 == 0 )
+    if ( (ibuffer.mode&1) == 0 )
         fs_remove(filesys, path);
     else {
         fs_close(filesys, fd);
-        fdir = fs_opendir(filesys, path);
+        fs_chdir(filesys, path);
+        fdir = fs_opendir(filesys, ".");
         while ( fs_nextent(fdir, entry, 999) )
         {
             if ( strcmp(entry, ".")!=0 && strcmp(entry, "..")!=0 )
+            {
                 remove_entry(entry);
+            }
         }
-        fs_close(fs, fdir);
-        fs_removedir(fs, path);
+        fs_closedir(fdir);
+        fs_chdir(filesys, "..");
+        fs_removedir(filesys, path);
     }
 }
 
 void rm(char* params[], int len)
 {
-    int dirp = 0;
     int i = 0;
-    int fd;
+    
     if ( len==0 ) { help("rm"); return; }
     for (; i<len; i++)
     {
-        remove_entry(parmas[i]);
+        remove_entry(params[i]);
     }
 }
 
@@ -122,8 +145,8 @@ static int sh_stat(const char* pathname, inode *ibuffer)
     int fd = fs_open(filesys, pathname, 0);
     if ( fd==-1 ) return -1;
 
-    r = fstat(fd, ibuffer);
-    fs_close(fd);
+    r = fs_fstat(filesys, fd, ibuffer);
+    fs_close(filesys, fd);
     return r;
 }
 
@@ -140,61 +163,73 @@ static int get_new_path(char* newp, const char* src, const char* dst)
     return len1+i-len2-1;
 }
 
+static int st_stat(const char* filename, inode* ibuffer)
+{
+    int r;
+    int fd = fs_open(filesys, filename, 0);
+
+    if ( fd==-1 ) return -1;
+    r = fs_fstat(filesys, fd, ibuffer);
+    fs_close(filesys, fd);
+    return r;
+}
+
 void cp(char* params[], int len)
 {
-    int fd1, fd2, len;
+    int fd1, fd2, len2;
     inode ibuffer;
 
     char* newp;
     char* buf;
     if ( len!= 2 ) { help("cp");return;}
-
-    if ( st_stat(params[0], &ibuffer)==-1 || ibuffer.imode&1 ) return;
-    if ( st_stat(params[1], &ibuffer )==-1 || ibuffer.imode&1==0 ) return;
-    if ( (fd1 = fs_open(params[0], FS_READ))==-1 ) return; 
+    
+    if ( st_stat(params[0], &ibuffer)==-1 || (ibuffer.mode&1) ) return;
+    if ( st_stat(params[1], &ibuffer )==-1 || (ibuffer.mode&1)==0 ) return;
+    if ( (fd1 = fs_open(filesys, params[0], FS_READ))==-1 ) return; 
     
     newp = (char*)malloc(1000);
-    get_new_path(newp, parmas[0], params[1]);
+    get_new_path(newp, params[0], params[1]);
 
     if ( sh_stat(newp, &ibuffer)!=-1 ) goto error;
-    if ( (fd2=fs_open(newp, FS_READ|FS_WRITE))==-1 ) goto error;
+    if ( (fd2=fs_open(filesys, newp, FS_READ|FS_WRITE))==-1 ) goto error;
 
-    char* buf = (char*)malloc(1000);
-    while ( len=fs_read(fs, fd1, buf, 1000) )
-        fs_write(fs, fd2, buf, len);
+    buf = (char*)malloc(1000);
+    while ( (len2=fs_read(filesys, fd1, buf, 1000))>0 )
+        fs_write(filesys, fd2, buf, len);
 
     free(buf);
-    fs_close(fd2);
+    fs_close(filesys, fd2);
 error:
-    fs_close(fd1);
+    fs_close(filesys, fd1);
     free(newp);
 }
 
 void get(char* params[], int len)
 {
-    FILE* fp_src, len;
+    FILE* fp_src;
     inode ibuffer;
-    int fd2;
+    int fd2, len2;
+    char* newp;
     
     if ( len!=2 ) { help("get"); return; }
 
-    if ( st_stat(params[1], &ibuffer )==-1 || ibuffer.imode&1==0 ) return;
+    if ( st_stat(params[1], &ibuffer )==-1 || (ibuffer.mode&1)==0 ) return;
     
-    FILE* fp_src =fopen(params[0], "rb");
+    fp_src =fopen(params[0], "rb");
     if ( fp_src==NULL ) return;
 
     newp = (char*)malloc(1000);
-    get_new_path(newp, parmas[0], params[1]);
+    get_new_path(newp, params[0], params[1]);
 
     if ( sh_stat(newp, &ibuffer)!=-1 ) goto error;
-    if ( (fd2=fs_open(newp, FS_READ|FS_WRITE))==-1 ) goto error;
+    if ( (fd2=fs_open(filesys, newp, FS_READ|FS_WRITE))==-1 ) goto error;
 
     char* buf = (char*)malloc(1000);
-    while ( len=fread(buf, 1, 1000, fp) )
-        fs_write(fs, fd2, buf, len);
+    while ( (len2=fread(buf, 1, 1000, fp_src))>0 )
+        fs_write(filesys, fd2, buf, len);
 
     free(buf);
-    fs_close(fd2);
+    fs_close(filesys, fd2);
 error:
     fclose(fp_src);
     free(newp);
@@ -202,39 +237,39 @@ error:
 
 void put(char* params[], int len)
 {
-    FILE* fp_dst, len;
+    FILE* fp_dst;
     inode ibuffer;
-    int fd;
+    int fd, len2;
+    char* newp;
+    
     if ( len!=2 ) { help("put"); return; }
 
-    if ( st_stat(params[0], &ibuffer )==-1 || ibuffer.imode&1 ) return;
+    if ( st_stat(params[0], &ibuffer )==-1 || ibuffer.mode&1 ) return;
     
-    if ( (fd=fs_open(fs, params[0], FS_READ))==-1 ) return;
+    if ( (fd=fs_open(filesys, params[0], FS_READ))==-1 ) return;
     
     newp = (char*)malloc(1000);
-    get_new_path(newp, parmas[0], params[1]);
+    get_new_path(newp, params[0], params[1]);
 
-    if ( ( fp_dst=fopen(newp, "wb") )==-1 ) goto error;
+    if ( ( fp_dst=fopen(newp, "wb") )==NULL ) goto error;
 
     char* buf = (char*)malloc(1000);
-    while ( len=fs_read(fs, fd, buf, 1000) )
-        fwrite(buf, 1, len, fp);
+    while ( (len2=fs_read(filesys, fd, buf, 1000))>0 )
+        fwrite(buf, 1, len, fp_dst);
 
     free(buf);
     fclose(fp_dst);
 error:
-    fs_close(fd);
+    fs_close(filesys, fd);
     free(newp);
 }
 
 const char* commands[]={"ls", "cd", "pwd", "mkdir", "rm", "cp", "get", "put", NULL};
-
-typedef void function(char* p[], int l);
+typedef void (*function)(char* p[], int l);
 function func[]={ls, cd, pwd, mkdir, rm, cp, get, put};
 
-fs* create_file_system(fs* f, int argc, char** argv)
+fs* create_file_system(int argc, char** argv)
 {
-   fs* temp_fs;
    int i, n_blks=0, n_inodes=0;
    char* fname=NULL;
    
@@ -245,24 +280,26 @@ fs* create_file_system(fs* f, int argc, char** argv)
            i++;
        }
        else if ( (strcmp(argv[i], "--size")==0 || strcmp(argv[i], "-s")==0) && i+1 < argc ){
-           n_blks = atoi(argv[i+1])
+           n_blks = atoi(argv[i+1]);
+           i++;
        }
        else if ( (strcmp(argv[i], "--inodes")==0 || strcmp(argv[i], "-i")==0) && i+1 < argc ){
-           n_indoes = atoi(argv[i+1]);
+           n_inodes = atoi(argv[i+1]);
+           i++;
        }
    }
 
-   if ( !fname || n_blks<=0 || n_inodes<=0 )
+   if ( !fname || n_blks<=0 || n_inodes<0 )
        return NULL;
    if ( n_inodes == 0 )
-       n_inodes == -1;
-   return create_file_system(temp_fs, n_blks, n_inodes);
+       n_inodes = -1;
+   return fs_creatfs(fname, n_blks, n_inodes);
 }
 
 int split_cmdline(char* options[], char* line)
 {
     int current = 0;
-    options[current++] = *line;
+    options[current++] = line;
     
     while (*line && *line!='\n')
     {
@@ -274,7 +311,7 @@ int split_cmdline(char* options[], char* line)
         }
         line++;
     }
-
+    if (*line=='\n') *line=0;
     return current;
 }
 
@@ -282,11 +319,11 @@ void parse_cmd(char* line)
 {
     static char* options[50];
     
-    int n_len = split_cmdline(options, cmdline);
+    int n_len = split_cmdline(options, line);
     int i;
     for (i=0; commands[i]; i++){
         if ( strcmp(commands[i], options[0])==0 )
-            func[i](options+1, n_len - 1);
+            return func[i](options+1, n_len - 1);
     }
     printf("sbsh:command not found.\n");
 }
@@ -294,14 +331,17 @@ void parse_cmd(char* line)
 void main_loop()
 {
     static char cmdline[1000];
-    
     printf("Welcome to shabby shell.\n");
+
+    fs_pwd(filesys, workdir, 999);
+    
     while ( 1 )
     {
-        printf("%s $", workdir);
-        if (gets(cmdline)==NULL)
+        printf("%s $ ", workdir);
+        if (fgets(cmdline, 1000, stdin)==NULL)
             goto error;
-        if ( strcmp(cmdline, "exit")==0 )
+        
+        if ( strcmp(cmdline, "exit\n")==0 )
             break;
         parse_cmd(cmdline);
     }
@@ -315,7 +355,6 @@ int main(int argc, char** argv)
     switch ( argc )
     {
     case 1:
-        print_help();
         break;
         
     case 2:
@@ -327,8 +366,10 @@ int main(int argc, char** argv)
     }
 
     if ( filesys == NULL )
-        print_help();
+        help("main");
     else {
         main_loop();
     }
+
+    return 0;
 }
